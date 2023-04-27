76ebcff3e639cbbea39758ceb392698d55c4f0f1
SkhundinMichail <113530482+SkhundinMichail@users.noreply.github.com>
2023-04-27 15:27:24 +0300

    avtotest12

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,13 +1,19 @@
 const cardSchema = require('../models/Card');
 
+const processingGoodResponse = (card, res) => {
+  if (card) {
+    res.status(200).send({ data: card });
+  } else {
+    res.status(404).send({ message: 'card not found, error 404' });
+  }
+};
+
 const processingError = (res, err) => {
   if (err.name === 'CastError') {
     res.status(400).send({ message: 'incorrect id, error 400' });
   } else if (err.name === 'ValidationError') {
     const message = Object.values(err.errors).map((error) => error.message).join(';');
     res.status(400).send({ message });
-  } else if (err.message === 'not_found') {
-    res.status(400).send({ message: 'card not found, error 400' });
   } else {
     res.status(500).send({ message: 'smth went wrong 500' });
   }
@@ -30,18 +36,18 @@ const createCard = (req, res) => {
 const deleteCard = (req, res) => {
   const { id } = req.params;
   cardSchema.findByIdAndRemove(id)
-    .then((card) => res.send({ data: card }))
+    .then((card) => { processingGoodResponse(card, res); })
     .catch((err) => processingError(res, err));
 };
 const likeCard = (req, res) => {
   // eslint-disable-next-line max-len
   cardSchema.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) => { res.status(201).send({ data: card }); })
+    .then((card) => { processingGoodResponse(card, res); })
     .catch((err) => processingError(res, err));
 };
 const dislikeCard = (req, res) => {
   cardSchema.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) => { res.status(200).send({ data: card }); })
+    .then((card) => { processingGoodResponse(card, res); })
     .catch((err) => processingError(res, err));
 };
 module.exports = {
